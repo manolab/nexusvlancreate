10c57bb408cdebbd1980a96f4f7ffc061c9bd30f
Normando <normando@micso.it>
2022-05-03 11:57:54 +0200

    Remove suppress-arp

--- createdestroyvxlan.py
+++ createdestroyvxlan.py
@@ -105,7 +105,7 @@ def add_l2vni_to_nve(l2vni, mcast_group):
     #clis.append("  host-reachability protocol bgp")
     clis.append("  member vni %s" % l2vni)
     clis.append("  mcast-group %s" % mcast_group)
-    clis.append("  suppress-arp")
+    #clis.append("  suppress-arp")
 
 def add_l2vni_to_evpn(l2vni):
     clis.append("evpn")
